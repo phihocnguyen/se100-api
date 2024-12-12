ffdbf8f81bf5a2e22b728a51d87b62100bd65076
mhndckngyn <126580562+mhndckngyn@users.noreply.github.com>
2024-12-12 13:35:30 +0700

    feat: Add Verification email sender

---
+++ src/helpers/verification-email-sender.ts
@@ -0,0 +1,25 @@
+import nodemailer from "nodemailer";
+require('dotenv').config({path: '../../.env'})
+
+const transporter = nodemailer.createTransport({
+  host: process.env.EMAIL_HOST,
+  port: Number(process.env.EMAIL_PORT),
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  }
+});
+
+const sendVerificationEmail = async (email: string, token: string) => {
+  const link = `http://localhost:${process.env.PORT}/user/verify?token=${token}`;
+  await transporter.sendMail({
+    from: process.env.EMAIL_USER,
+    to: email,
+    subject: "E-ShopHub Verification email",
+    html: `<p>Please click the link below to verify your email:</p><a href="${link}">${link}</a>\`,`
+  })
+}
+
+sendVerificationEmail("ngdm271@gmail.com", "123");
+
+export default sendVerificationEmail
