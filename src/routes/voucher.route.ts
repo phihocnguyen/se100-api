079601856db1a1f219ef9c1a03359687686d4ba2
BillyBat04 <khoafd11@gmail.com>
2025-01-12 22:41:06 +0700

    Update voucher.route.ts

--- src/routes/voucher.route.ts
+++ src/routes/voucher.route.ts
@@ -1,14 +1,14 @@
 import { Router } from "express";
-import UserController from "../controllers/user.controller";
+
 import upload from "../middleware/multer";
-import CustomerController from "../controllers/customer.controller";
+import VoucherController from "../controllers/voucher.controller";
 
 class VoucherRoute {
-    private readonly customerController: CustomerController
+    private readonly customerController: VoucherController
     public readonly router: Router
 
     constructor() {
-        this.customerController = new CustomerController()
+        this.customerController = new VoucherController()
         this.router = Router()
         this.router.post('/', this.customerController.create.bind(this.customerController))
         this.router.get('/', this.customerController.create.bind(this.customerController))
